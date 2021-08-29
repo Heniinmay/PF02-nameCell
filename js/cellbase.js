7052c8e70b1095eee8e09349ee7863104bd97f7f
heni <realhayley05@gmail.com>
2021-08-29 23:18:04 +0900

    aside debugging

--- js/cellbase.js
+++ js/cellbase.js
@@ -1,18 +1,23 @@
 AOS.init();
-const sidemenu = document.querySelector('.sidemenu');
-const menuAll = sidemenu.querySelector('.menu_all');
-const gnbWrap = document.querySelector('.gnb-wrap');
 
-menuAll.addEventListener('click', function () {
-    sidemenu.classList.toggle('active');
-    menuAll.classList.toggle('active');
-    gnbWrap.classList.toggle('active');
+//aside
+
+let sidemenu = document.querySelector(".sidemenu");
+let menuAll = sidemenu.querySelector(".menu_all");
+let gnbWrap = document.querySelector(".gnb-wrap");
+
+menuAll.addEventListener("click", function () {
+  sidemenu.classList.toggle("active");
+  menuAll.classList.toggle("active");
+  gnbWrap.classList.toggle("active");
 });
 
-let sideMenu = $('.sidemenu'),
-    sdMenuLG = sideMenu.find('.sidelogo');
+let sideMenu = $(".sidemenu"),
+  sdMenuLG = sideMenu.find(".sidelogo");
 
 sdMenuLG.click(function () {
-    //$(this).attr('href','./index.html'); //temp
-    sideMenu.find('a.sidelogo').attr('href', 'http://yumgme.dothome.co.kr/celltrion');
-})
+  //$(this).attr('href','./index.html'); //temp
+  sideMenu
+    .find("a.sidelogo")
+    .attr("href", "http://yumgme.dothome.co.kr/celltrion");
+});
